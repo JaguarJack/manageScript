ccf1dc3b404bc9c4ad465a45dbdfefa7dd2764b6
yanwenwu <82664165@qq.com>
2017-07-19 15:18:28 +0800

    加入脚本引入

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ if ($argc < 2) exit('please input your start script name' . PHP_EOL);
 try {
     require './core/start.php';
     
+    //引入基类
+    include_once ROOT_PATH . DS . 'scripts' .DS. 'Base' . EXT;
+    //引入脚本文件
+    include_once ROOT_PATH . DS . 'scripts' .DS. $argv[1] . EXT;
     $class_name = strtoupper($argv[1]);
     if (class_exists($class_name)) {
         $class = new $class_name;
@@ -12,9 +16,8 @@ try {
         exit('Class Not Found');
     }
 } catch (\Exception $e) {
-    echo $e->getMessage();
-    echo $e->getFile();
-    echo $e->getFile();
+    echo $e->getMessage();    
+    echo $e->getTraceAsString();
 }
 
 
