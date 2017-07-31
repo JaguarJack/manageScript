97d035adbf130c9ed1357978bb8570a19ec19bab
yanwenwu <82664165@qq.com>
2017-07-31 21:26:35 +0800

    删除配置项

--- config/config.php
+++ config/config.php
@@ -8,14 +8,18 @@ return [
     
     'master' => [
         'host'  => '127.0.0.1',
-        'port'  => 9999,
+        'port'  => 9008,
         'daeme' => 1,
-        'worker_num' => 4,
+        'worker_num' => 10,
         'pid_file'  => LOG_PATH . 'master.pid',
         'log_path'  => LOG_PATH . 'swoole.log',
         'log_level' => 0,
-        'is_allow_repeat' => 0,
         'is_kill_process' => true,//主进程结束，是否结束所有子进程
         'is_reload_process' => false,//子进程结束后是否重新拉起
+    ],
+    
+    'process' => [
+        'worker_num' => 10,
+        'is_reload_proccess' => true,
     ],
 ];
