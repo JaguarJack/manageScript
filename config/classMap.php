8e5c6c3f3137a56e2eefadc7244f3056ffa15266
wuyanwen <82664165@qq.com>
2017-08-18 17:07:05 +0800

    添加命名空间

--- config/classMap.php
+++ config/classMap.php
@@ -1,5 +1,6 @@
 <?php
 return [
-    'Vendor\\' => ROOT_PATH . 'vendor' . DS,
+    'Vendor\\' => ROOT_PATH . 'vendor'  . DS,
     'Cron\\'   => ROOT_PATH . 'crontab' . DS,
+    'Script\\' => ROOT_PATH . 'scripts'  . DS,
 ];
