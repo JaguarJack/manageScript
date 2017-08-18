cd3d9a8c6850b410479cc2c619658e84f41d1138
wuyanwen <82664165@qq.com>
2017-08-18 17:13:42 +0800

    修改入口

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
-if ($argc < 2) exit('please input your start script name' . PHP_EOL);
 
-try {
-    require __DIR__ . '/core/start.php';
-    
-    //引入基类
-    include_once ROOT_PATH . 'scripts' .DS. 'Base' . EXT;
-    //引入脚本文件
-    include_once ROOT_PATH . 'scripts' .DS. $argv[1] . EXT;
-    $class_name = strtoupper($argv[1]);
-    if (class_exists($class_name)) {
-        $class = new $class_name;
-        $class->exec();
-    } else {
-        exit('Class Not Found');
-    }
-} catch (\Exception $e) {
-    echo $e->getMessage();    
-    echo $e->getTraceAsString();
-}
+include __DIR__ . '/core/start.php';
+
+$app = new Core\Cen\App('Script');
+
+$app->run($argv);
 
 
 
