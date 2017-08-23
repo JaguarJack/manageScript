849df082dc7c23858d1297adee0284eb57146405
wuyanwen <82664165@qq.com>
2017-08-23 17:30:28 +0800

    优化Log类

--- core/class/Log.php
+++ core/class/Log.php
@@ -15,46 +15,49 @@ class Log
     const INFO      = 'info';
     const DEBUG     = 'debug';
     const LOGEXT    = '.log';
-    
+    private static $log_config = null;
+    private static $file = null;
+    private static $log_num = 1;
+    private static $queue = null;
     /**
      * @description:日志写入
      * @author wuyanwen(2017年7月11日)
      * @param unknown $level
      * @param unknown $info
      */
-    public static function write($level,$info)
+    public static function write($level, $info, $type = '')
     {
-        call_user_func_array([__CLASS__,$level],[$info]);       
+        call_user_func_array([__CLASS__,$level],[$info, $type]);       
     }
     
-    private static function emergency($info)
+    private static function emergency($info, $type)
     {    
-        self::record(__FUNCTION__, $info); 
+        self::record(__FUNCTION__, $info, $type); 
     }
     
-    private static function alert($info)
+    private static function alert($info, $type)
     {
-        self::record(__FUNCTION__, $info);
+        self::record(__FUNCTION__, $info, $type);
     }
     
-    private static function error($info)
+    private static function error($info, $type)
     {
-        self::record(__FUNCTION__, $info);
+        self::record(__FUNCTION__, $info, $type);
     }
     
-    private static function warning($info)
+    private static function warning($info, $type)
     {
-        self::record(__FUNCTION__, $info);
+        self::record(__FUNCTION__, $info, $type);
     }
     
-    private static function notice($info)
+    private static function notice($info, $type)
     {
-        self::record(__FUNCTION__, $info);
+        self::record(__FUNCTION__, $info, $type);
     }
     
-    private static function info($info)
+    private static function info($info, $type)
     {
-        self::record(__FUNCTION__, $info);
+        self::record(__FUNCTION__, $info, $type);
     }
     
     private static function debug($info)
@@ -62,28 +65,54 @@ class Log
         self::record(__FUNCTION__, $info);
     }
     
-    private static function record($level,$info)
+    /**
+     * @description:写入日志
+     * @author wuyanwen(2017年8月23日)
+     * @param unknown $level
+     * @param unknown $info
+     * @param string $type
+     */
+    private static function record($level, $info, $type)
     {
-        $log_config = Config::get('config')['log'];
-        $daily_log_path   = $log_config['path'] . date('Y_m_d') . '/'; 
-
-        $file = new File;
+        if (!self::$log_config) {
+            self::$log_config =  Config::get('config.log');
+        }
+        
+        $daily_log_path   = self::$log_config['path'] . date('Y_m_d') . '/'; 
+       
+        if (!self::$file) {
+            self::$file = new File;
+        }
         //创建每日日志目录
-        if ( !$file->isDirectory($daily_log_path))
-            $file->mkDirectory($daily_log_path);        
-        //匹配所有日志文件
-        $logs_arr = $file->glob($daily_log_path . '*' .self::LOGEXT); 
-        $logs_num = count($logs_arr);
-        //创建日志文件
-        $log_file = $daily_log_path . ( $logs_num ? $logs_num : 1 ) . self::LOGEXT;
-        //如果超过配置的日志文件大小，则重新创建文件
-        if ($file->exists($log_file) && $file->fileSize($log_file) > $log_config['size']) 
-            $log_file = $daily_log_path . ($logs_num + 1) . self::LOGEXT;
-
-        //文件资源
+        if ( !self::$file->isDirectory($daily_log_path)) {
+            self::$file->mkDirectory($daily_log_path); 
+        }
+        
+        //日志记录
         $message = sprintf('[ %s ]%s : %s' . PHP_EOL, date('Y-m-d H:i:s'), strtoupper($level), $info);
-        //写入日志
-        $file->open($log_file, 'a')->write($message, LOCK_EX);
+        
+        if ($type) {
+            $log_file = $daily_log_path . $type . self::LOGEXT;
+            self::$file->open($log_file, 'a')->write($message, LOCK_EX);
+            return true;
+        }
 
+        //创建日志文件
+         $log_file = $daily_log_path . self::$log_num . self::LOGEXT;
+        
+        //如果超过配置的日志文件大小，则重新创建文件
+        if (self::$file->exists($log_file) && self::$file->fileSize($log_file) > self::$log_config['size']) {
+            //匹配所有日志文件
+            $logs_arr = self::$file->glob($daily_log_path . '*' .self::LOGEXT);
+            self::$log_num = count($logs_arr);
+            $log_file = $daily_log_path . self::$log_num . self::LOGEXT;
+            if (self::$file->exists($log_file) && self::$file->fileSize($log_file) > self::$log_config['size']) {
+                $log_file = $daily_log_path . (self::$log_num + 1) . self::LOGEXT;
+            }
+        }
+        
+        //写入日志
+        self::$file->open($log_file, 'a')->write($message, LOCK_EX);
     }
+
 }
