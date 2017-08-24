f1904cc8d758ef40e03fdfb44405d65dcfa3b1f9
wuyanwen <82664165@qq.com>
2017-08-24 17:07:49 +0800

    增加助手函数

--- helper/helper.php
+++ helper/helper.php
@@ -17,3 +17,22 @@ function config($key, $value = '')
     return Config::get($key);
 }
 
+/**
+ * @description:将秒转换成 时分秒
+ * @author wuyanwen(2017年8月24日)
+ * @param unknown $end_time
+ */
+function secondsToHis($end_time)
+{
+    $seconds = ceil($end_time - TASK_START);
+    
+    if ($seconds < 60) {
+        return $seconds . 'S';
+    }
+    
+    if ($seconds < 3600) {
+        return gmstrftime('%MMin%SS', $seconds);
+    }
+    
+    return gmstrftime('%HHour%MMin%sS', $seconds);
+}
